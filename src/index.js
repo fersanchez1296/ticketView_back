76462261782f1ef4ff5ea6cec31f59b423cb0fc7
fergob <fernando.sanchezplascencia@jalisco.gob.mx>
2025-01-16 11:43:04 -0600

    cambios en el process.env para la firma del token

--- src/index.js
+++ src/index.js
@@ -45,5 +45,6 @@ redisClient.connect().then(() => {
   console.log(__dirname);
   console.log("Redis connected");
   console.log(`Server running on port ${process.env.BACKEND_PORT}`);
+  console.log(`Server running on port ${process.env.JWT_TOKEN}`);
   app.listen(process.env.BACKEND_PORT);
 });
--- src/middleware/verifyToken.middleware.js
+++ src/middleware/verifyToken.middleware.js
@@ -1,5 +1,4 @@
 import jwt from "jsonwebtoken";
-import "dotenv/config";
 export const verifyToken = (req, res, next) => {
     const token = req.cookies.access_token;
     if(!token) {
