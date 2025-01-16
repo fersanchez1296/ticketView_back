2cf15ac46575de5cab75230d522a73b456bf78bb
fergob <fernando.sanchezplascencia@jalisco.gob.mx>
2025-01-16 10:59:25 -0600

    cambios en la conexion a redis

--- src/config/redis_connection.js
+++ src/config/redis_connection.js
@@ -3,6 +3,6 @@ import redis from "redis"
 
 // Crear un cliente Redis
 export const redisClient = redis.createClient({
-  url: `redis://${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`,
+  url: `redis://redis:6379`,
 });
 redisClient.on("error", (err) => console.log("Redis Client Error", err));
