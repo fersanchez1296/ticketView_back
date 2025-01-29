faebff117f02181f6eeed0103c6927ce24a7d4a8
fergob <fernando.sanchezplascencia@jalisco.gob.mx>
2025-01-29 11:09:11 -0600

    se elimino la causa de cierre del esquema

--- src/schemas/cerrarTicket.schema.js
+++ src/schemas/cerrarTicket.schema.js
@@ -3,7 +3,6 @@ const cerrarSchema = Joi.object({
   Files: Joi.object().allow(null, ""),
   Descripcion_cierre: Joi.string().required(),
   Numero_Oficio: Joi.string().allow("").required(),
-  Causa: Joi.string().required(),
 });
 
 export default cerrarSchema;
