2f43eb5495c5a5dc1d841e904b0d6e732f21af1c
fergob <fernando.sanchezplascencia@jalisco.gob.mx>
2025-02-27 13:39:45 -0600

    se modifico el controlador de crear ticket y editar ticket para aceptar el campo de medio de contacto. El campo medio de contacto se agrego a los modelos. Se modifico el middleware de formatear fechas para regresar una cadena vacia cuando contiene la fecha por defecto

--- src/middleware/formatearFechas.middleware.js
+++ src/middleware/formatearFechas.middleware.js
@@ -4,40 +4,31 @@ import { es } from "date-fns/locale";
 export const formatearCamposFecha = (req, res, next) => {
   try {
     const DATA = req.tickets.map((ticket) => {
+      const formatFecha = (fecha) => {
+        if (!fecha || new Date(fecha).getFullYear() === 1900) {
+          return "";
+        }
+        return format(fecha, "d 'de' MMMM 'de' yyyy, h:mm a", { locale: es });
+      };
+
       return {
         ...ticket,
-        Fecha_hora_creacion: format(
-          ticket.Fecha_hora_creacion,
-          "d 'de' MMMM 'de' yyyy, h:mm a",
-          { locale: es }
+        Fecha_hora_creacion: formatFecha(ticket.Fecha_hora_creacion),
+        Fecha_limite_resolucion_SLA: formatFecha(
+          ticket.Fecha_limite_resolucion_SLA
         ),
-        Fecha_limite_resolucion_SLA: format(
-          ticket.Fecha_limite_resolucion_SLA,
-          "d 'de' MMMM 'de' yyyy, h:mm a",
-          { locale: es }
+        Fecha_hora_ultima_modificacion: formatFecha(
+          ticket.Fecha_hora_ultima_modificacion
         ),
-        Fecha_hora_ultima_modificacion: format(
-          ticket.Fecha_hora_ultima_modificacion,
-          "d 'de' MMMM 'de' yyyy, h:mm a",
-          { locale: es }
-        ),
-        Fecha_hora_cierre: format(
-          ticket.Fecha_hora_cierre,
-          "d 'de' MMMM 'de' yyyy, h:mm a",
-          { locale: es }
-        ),
-        Fecha_limite_respuesta_SLA: format(
-          ticket.Fecha_limite_respuesta_SLA,
-          "d 'de' MMMM 'de' yyyy, h:mm a",
-          { locale: es }
+        Fecha_hora_cierre: formatFecha(ticket.Fecha_hora_cierre),
+        Fecha_limite_respuesta_SLA: formatFecha(
+          ticket.Fecha_limite_respuesta_SLA
         ),
         Historia_ticket: ticket.Historia_ticket
           ? ticket.Historia_ticket.map((historia) => ({
               Nombre: historia.Nombre,
               Mensaje: historia.Mensaje,
-              Fecha: format(historia.Fecha, "d 'de' MMMM 'de' yyyy, h:mm a", {
-                locale: es,
-              }),
+              Fecha: formatFecha(historia.Fecha),
             }))
           : [],
       };
