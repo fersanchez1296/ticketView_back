bddfce53774d88bd2648ea3263dc292b7469519c
fergob <fernando.sanchezplascencia@jalisco.gob.mx>
2025-03-05 09:52:49 -0600

    se modifico la forma en la que se obtiene el correo del moderador en el controlador regresarCorreos

--- src/controllers/ticket.controller.js
+++ src/controllers/ticket.controller.js
@@ -1031,7 +1031,7 @@ export const regresarcorreos = async (req, res) => {
     }
     const CORREOS = {
       correoCliente: Datos.Cliente.Correo,
-      correoModerador: Datos.Asignado_a.Correo,
+      correoModerador: Datos.Asignado_a[0].Correo,
       correoMesa: process.env.SMTP_USERNAME,
     };
     console.log("CORREOS", CORREOS);
