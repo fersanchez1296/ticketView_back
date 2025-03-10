e560fe8c46a37cc9cc2cbf52ca6c751dc583b6fd
fergob <fernando.sanchezplascencia@jalisco.gob.mx>
2025-03-09 20:54:52 -0600

    modificacion en la funcion de utils que calcula si un ticket fue resuelto a tiempo o no

--- src/utils/ticketEnTiempo.js
+++ src/utils/ticketEnTiempo.js
@@ -1,5 +1,5 @@
 import { isAfter } from "date-fns";
-export default function incTicketsUsuario(fecha_limite, fecha_resolucion) {
+export default function incTicketsUsuario(fecha_resolucion, fecha_limite) {
   const resuelto = isAfter(fecha_resolucion,fecha_limite);
   return resuelto ? "fuera_tiempo" : "a_tiempo";
 }
