9e1bc3e903f6546d2dedad52d3cd7eb8557afd6a
fergob <fernando.sanchezplascencia@jalisco.gob.mx>
2025-04-10 10:27:13 -0600

    se agregaron las rutas y controladores para la busqueda avanzada

--- src/controllers/dashboard.controller.js
+++ src/controllers/dashboard.controller.js
@@ -1,5 +1,6 @@
 import { TICKETS } from "../models/index.js";
 import mongoose from "mongoose";
+import * as Gets from "../repository/gets.js";
 const ObjectId = mongoose.Types.ObjectId;
 export const dashboard = async (req, res) => {
   const { userId, areas } = req.session.user;
@@ -104,4 +105,134 @@ export const dashboard = async (req, res) => {
       //sales,
     });
   } catch (error) {}
+};
+
+export const general = async (req, res, next) => {
+  try {
+    const { termino } = req.query;
+    console.log(termino);
+    const orConditions = [
+      { Descripcion: { $regex: termino, $options: "i" } },
+      { Id: req.id },
+      { NumeroRec_Oficio: { $regex: termino, $options: "i" } },
+      { Numero_Oficio: { $regex: termino, $options: "i" } },
+    ];
+    if (req.cliente && req.cliente.length > 0) {
+      const Cliente = [];
+      req.cliente.forEach((element) => {
+        Cliente.push(...element.ids);
+      });
+      orConditions.push({ Cliente: Cliente });
+    }
+    if (req.usuario && req.usuario.length > 0) {
+      req.usuario.forEach((element) => {
+        const field = element.field;
+        const value = element.ids;
+        const objectIds = value.map((id) => new mongoose.Types.ObjectId(id));
+        orConditions.push({ [field]: { $in: objectIds } });
+      });
+    }
+    if (req.area && req.area.length > 0) {
+      req.area.forEach((element) => {
+        const field = element.field;
+        const value = element.ids;
+        const objectIds = value.map((id) => new mongoose.Types.ObjectId(id));
+        orConditions.push({ [field]: { $in: objectIds } });
+      });
+    }
+    if (req.subcategoria && req.subcategoria.length > 0) {
+      req.subcategoria.forEach((element) => {
+        const field = element.field;
+        const value = element.ids;
+        const objectIds = value.map((id) => new mongoose.Types.ObjectId(id));
+        orConditions.push({ [field]: { $in: objectIds } });
+      });
+    }
+    console.log(orConditions);
+    const result = await Gets.getTicketsGeneral(orConditions);
+    if (!result || result.length === 0) {
+      return res.status(404).json({
+        desc: "No se encontraron tickets con este termino de busqueda.",
+      });
+    }
+    req.tickets = result;
+    return next();
+  } catch (error) {
+    return res.status(500).json({
+      desc: "Error al buscar tickets. Error interno en el servidor.",
+    });
+  }
+};
+
+export const oficio = async (req, res, next) => {
+  try {
+    const { termino } = req.query;
+    const result = await Gets.getTicketsPorOficio(termino);
+    if (!result) {
+      return res.status(404).json({
+        desc: "No se encontraron tickets con este termino de busqueda.",
+      });
+    }
+    req.tickets = result;
+    return next();
+  } catch (error) {
+    return res.status(500).json({
+      desc: "Error al buscar tickets por oficio. Error interno en el servidor.",
+    });
+  }
+};
+
+export const nccliente = async (req, res, next) => {
+  if (req.cliente.length === 0) {
+    return res.status(404).json({
+      desc: "No se encontro un cliente con este termino de busqueda",
+    });
+  }
+  try {
+    const Cliente = [];
+    req.cliente.forEach((element) => {
+      Cliente.push(...element.ids);
+    });
+    const result = await Gets.getTicketsPorCliente(Cliente);
+    if (!result) {
+      return res.status(404).json({
+        desc: "No se encontraron tickets con este termino de busqueda.",
+      });
+    }
+    req.tickets = result;
+    return next();
+  } catch (error) {
+    return res.status(500).json({
+      desc: "No se puedo encotrar un cliente con estos terminos. Error interno en el servidor.",
+    });
+  }
+};
+
+export const ncresolutor = async (req, res, next) => {
+  if (req.usuario.length === 0) {
+    return res.status(404).json({
+      desc: "No se encontro un resolutor con este termino de busqueda",
+    });
+  }
+  try {
+    const orConditions = [];
+    req.usuario.forEach((element) => {
+      const field = element.field;
+      const value = element.ids;
+      const objectIds = value.map((id) => new mongoose.Types.ObjectId(id));
+      orConditions.push({ [field]: { $in: objectIds } });
+    });
+    const result = await Gets.getTicketsPorUsuario(orConditions);
+    if (!result) {
+      return res.status(404).json({
+        desc: "No se encontraron tickets con este termino de busqueda.",
+      });
+    }
+    req.tickets = result;
+    return next();
+  } catch (error) {
+    return res.status(500).json({
+      desc: "No se puedo encotrar un resolutor con estos terminos. Error interno en el servidor.",
+    });
+  }
 };
